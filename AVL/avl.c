81206188cf945ed453db1b70cfdbea6b5ce47848
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2022-12-17 20:48:50 +0300

    AVL FULL

--- AVL/avl.c
+++ AVL/avl.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+typedef struct node {
     int data, height;
     struct node *left, *right;
-};
+} AVLTREE;
 
-typedef struct node AVLTREE;
+int height(AVLTREE *root){
 
-AVLTREE *new_node(int number) {
-    AVLTREE *node = (AVLTREE *)malloc(sizeof(AVLTREE));
-
-    node->data = number;
-    node->left = node->right = NULL;
-    node->height = 0;
-
-    return node;
+    if (root == NULL)
+        return -1;
+    else {
+        int left_height, right_height;
+        left_height = height(root->left);
+        right_height = height(root->right);
+        if (left_height > right_height)
+            return 1 + left_height;
+        else
+            return 1 + right_height;
+    }
 }
 
-int maxValue(int x, int y) {
-    return x >= y ? x : y;
+int max(int x, int y) {
+    return x > y ? x : y;
 }
 
 AVLTREE *rightRotate(AVLTREE *z) {
@@ -27,10 +30,10 @@ AVLTREE *rightRotate(AVLTREE *z) {
     z->left = temp->right;
     temp->right = z;
 
-    z->height = maxValue(z->left == NULL ? -1 : z->left->height, z->right == NULL ? -1 : z->right->height) + 1;
-    temp->height = maxValue(temp->left == NULL ? -1 : temp->left->height, temp->right == NULL ? -1 : temp->right->height) + 1;
+    z->height = max(height(z->left), height(z->right));
+    temp->height = max(height(temp->left), height(temp->right));
 
-    return temp; 
+    return temp;
 }
 
 AVLTREE *leftRotate(AVLTREE *z) {
@@ -38,79 +41,126 @@ AVLTREE *leftRotate(AVLTREE *z) {
     z->right = temp->left;
     temp->left = z;
 
-    z->height = maxValue(z->left == NULL ? -1 : z->left->height, z->right == NULL ? -1 : z->right->height) + 1;
-    temp->height = maxValue(temp->left == NULL ? -1 : temp->left->height, temp->right == NULL ? -1 : temp->right->height) + 1;
+    z->height = max(height(z->left), height(z->right));
+    temp->height = max(height(temp->left), height(temp->right));
 
     return temp;
 }
 
-AVLTREE *leftRightRotate(AVLTREE *z) {
-    z->left = leftRotate(z->left);
-
-    return rightRotate(z);
+int getBalance(AVLTREE *root) {
+    if (root == NULL)
+        return 0;
+    return height(root->left) - height(root->right);
 }
 
-AVLTREE *rightLeftRotate(AVLTREE *z) {
-    z->right = rightRotate(z->right);
+AVLTREE *new_node(int key) {
+    AVLTREE *temp = (AVLTREE *)malloc(sizeof(AVLTREE));
 
-    return leftRotate(z);
+    temp->data = key;
+    temp->left = temp->right = NULL;
+    temp->height = 0;
+    
+    return temp;
 }
 
-AVLTREE *insertAVL(AVLTREE *tree, int x) {
-    if (tree == NULL)
-        tree = new_node(x);
+AVLTREE *insertAVL(AVLTREE *root, int key) {
+    if (root == NULL)
+        root = new_node(key);
     else {
-        if (x < tree->data)
-            tree->left = insertAVL(tree->left, x);
+        if (key < root->data)
+            root->left = insertAVL(root->left, key);
+        else if (key > root->data)
+            root->right = insertAVL(root->right, key);
         else
-            tree->right = insertAVL(tree->right, x);
+            return root;
 
-        tree->height = maxValue(tree->left == NULL ? -1 : tree->left->height, tree->right == NULL ? -1 : tree->right->height);
+        root->height = 1 + max(height(root->left), height(root->right));
 
-        if (((tree->left == NULL ? -1 : tree->left->height) - (tree->right == NULL ? -1 : tree->right->height)) > 1 && x < tree->left->data)
-            return rightRotate(tree);
+        int balance = getBalance(root);
 
-        if (((tree->left == NULL ? -1 : tree->left->height) - (tree->right == NULL ? -1 : tree->right->height)) > 1 && x > tree->left->data)
-            return leftRightRotate(tree);   
-
-        if (((tree->left == NULL ? -1 : tree->left->height) - (tree->right == NULL ? -1 : tree->right->height)) < -1 && x > tree->left->data)
-            return leftRotate(tree);
-
-        if (((tree->left == NULL ? -1 : tree->left->height) - (tree->right == NULL ? -1 : tree->right->height)) < -1 && x < tree->left->data)
-            return rightLeftRotate(tree);   
+        if (balance > 1 && key < root->left->data)
+            return rightRotate(root);
+        if (balance < -1 && key > root->right->data)
+            return leftRotate(root);
+        if (balance > 1 && key > root->left->data) {
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+        }
+        if (balance < -1 && key < root->right->data){
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
     }
 
-    return tree;
+    return root;
 }
 
-void preOrder(AVLTREE *root)
-{
-    if (root != NULL)
-    {
-        printf("%4d", root->data);
-        preOrder(root->left);
-        preOrder(root->right);
-    }   
+AVLTREE *minValue(AVLTREE *root) {
+    AVLTREE *temp = root;
+
+    while (temp->left != NULL)
+        temp = temp->left;
+
+    return temp;
 }
 
-void inOrder(AVLTREE *root)
-{
-    if (root != NULL)
-    {
-        inOrder(root->left);
-        printf("%4d", root->data);
-        inOrder(root->right);
-    }   
+AVLTREE *delete(AVLTREE *root, int key){
+    if (root == NULL)
+        return NULL;
+    if (key < root->data)
+        root->left = delete(root->left, key);
+    else if (key > root->data)
+        root->right = delete(root->right, key);
+    else {
+        // No child or only one child
+        if (root->left == NULL || root->right == NULL)
+        {
+            AVLTREE *temp = root->left ? root->left : root->right;
+
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            } else 
+                *root = *temp;
+            free(temp);
+        } else { // There are 2 children
+            AVLTREE *temp = minValue(root->right);
+
+            root->data = temp->data;
+
+            root->right = delete(root->right, temp->data);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    root->height = 1 + max(height(root->left), height(root->right));
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+    if (balance > 1 && getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if (balance < -1 && getBalance(root->right) > 0){
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
 }
 
-void postOrder(AVLTREE *root)
-{
-    if (root != NULL)
-    {
-        postOrder(root->left);
-        postOrder(root->right);
-        printf("%4d", root->data);
-    }   
+void inOrder(AVLTREE *root) {
+    if (root == NULL)
+        return ;
+    inOrder(root->left);
+    printf("%4d", root->data);
+    inOrder(root->right);
 }
 
 int main()
@@ -122,7 +172,7 @@ int main()
 
     while (program != 0)
     {
-        printf("\n\nPlease choose a program:\n1) Insert data to the AVL.\n2) Get the height of the AVL.\n3) Print inorderly.\n0) Exit.\n");
+        printf("\n\nPlease choose a program:\n1) Insert data to the AVL.\n2) Get the height of the AVL.\n3) Delete data.\n4) Print inorderly.\n0) Exit.\n");
         scanf("%d", &program);
 
         switch (program)
@@ -139,6 +189,13 @@ int main()
             break;
         
         case 3:
+            printf("Deleting data-->\nEnter an integer: ");
+            scanf("%d", &x);
+
+            myroot = delete(myroot, x);
+            break;
+
+        case 4:
             printf("Printing the AVL-->\n");
             inOrder(myroot);
             break;
@@ -147,8 +204,6 @@ int main()
             break;
         }
     }
-    
-    
 
     return 0;
 }
